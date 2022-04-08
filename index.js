bac1ae2232cdbb6232d4caa4797027cdf20a6ac6
Marcelo-Diament <marcelodiament@gmail.com>
2022-04-08 00:51:31 -0300

    feat (aula 01): atividade 03 - etapa 04 - criação de rota POST

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3333
 
 app.get('/usuario', (req, res) => res.send('<h1>Rota GET para "/usuario"</h1>'))
+app.post('/usuario', (req, res) => res.send('<h1>Rota POST para "/usuario"</h1>'))
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
