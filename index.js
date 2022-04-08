dac08495e1f77fb744b847e3c26c30936f0dd34a
Marcelo-Diament <marcelodiament@gmail.com>
2022-04-08 01:13:33 -0300

    feat (aula 01): atividade 03 - etapa 06 - sobre a rota para exclusão de usuário DELETE

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const port = 3333
 app.get('/usuario', (req, res) => res.send('<h1>Rota GET para "/usuario"</h1>'))
 app.post('/usuario', (req, res) => res.send('<h1>Rota POST para "/usuario"</h1>'))
 app.put('/usuario/:id', (req, res) => res.send(`<h1>Rota PUT para "/usuario" de id ${req.params.id}</h1>`))
+app.delete('/usuario/:id', (req, res) => res.send(`<h1>Rota DELETE para "/usuario" de id ${req.params.id}</h1>`))
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
