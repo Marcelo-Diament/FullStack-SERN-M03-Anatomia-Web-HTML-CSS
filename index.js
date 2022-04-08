f073471a30b7863c48d76ec204be192d4a3f035d
Marcelo-Diament <marcelodiament@gmail.com>
2022-04-07 23:19:30 -0300

    feat (aula 01): atividade 03 - etapa 03 - criação de rota GET

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require("express")
 const app = express()
 const port = 3333
 
-app.listen(port, () => `Servidor rodando na porta ${port}`)
+app.get('/usuario', (req, res) => res.send('<h1>Rota GET para "/usuario"</h1>'))
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
