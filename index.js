6bc59fb299984b0d02d3a21d33d029f7ff26fe77
Marcelo-Diament <marcelodiament@gmail.com>
2022-04-08 01:54:38 -0300

    feat (aula 01): desafio - parâmetros opcionais

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = 3333
 
-app.get('/usuario', (req, res) => res.send('<h1>Rota GET para "/usuario"</h1>'))
+app.get('/usuario/:id?', (req, res) => req.params.id ? res.send(`<h1>Rota GET para "/usuario/${req.params.id}"</h1>`) : res.send(`<h1>Rota GET para "/usuario"</h1>`))
 app.post('/usuario', (req, res) => res.send('<h1>Rota POST para "/usuario"</h1>'))
 app.put('/usuario/:id', (req, res) => res.send(`<h1>Rota PUT para "/usuario" de id ${req.params.id}</h1>`))
 app.delete('/usuario/:id', (req, res) => res.send(`<h1>Rota DELETE para "/usuario" de id ${req.params.id}</h1>`))
